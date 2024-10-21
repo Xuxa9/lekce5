ebafab3d8cd2ded0b3f55c66c21aa1e8379c6f24
Zuzana Čechová <zu.cechova9@gmail.com>
2024-10-21 18:31:59 +0200

    prvni commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const nadpis = document.querySelector("h1")
+// const nadpis2 = document.querySelectorAll("h1")
+
+nadpis.textContent = "Toto jsem zmenil za pomoci JS"
+
+nadpis.style.color = "yellow"
+nadpis.style.backgroundColor = "red"
+
+nadpis.classList.add("ramecek")
